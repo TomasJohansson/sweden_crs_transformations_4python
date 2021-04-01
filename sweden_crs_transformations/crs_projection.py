fad8ada2e220b6aeff53f1070427752981821402
Tomas Johansson <github-tomas@programmerare.com>
2021-04-01 23:28:47 +0200

    Only indentation. Nothing else in this git commit.

--- sweden_crs_transformations/crs_projection.py
+++ sweden_crs_transformations/crs_projection.py
@@ -13,95 +13,95 @@
 """
 
 """
-    /// <summary>
-    /// Crs = Coordinate reference system.
-    ///
-    /// The integer values for these enums are the EPSG numbers for the corresponding coordinate reference systems.
-    /// There are three kind of coordinate systems supported and defined in this enum type below:
-    ///     WGS84
-    ///     SWEREF99 (the new Swedish grid, 13 versions, one national grid and 12 local projection zones)
-    ///     RT90 (the old Swedish grid, 6 local projection zones)
-    /// There are extensions methods for the enum which can be used to determine one of the above three types.
-    /// See also <see cref="CrsProjectionExtensions"/>
-    ///
-    /// Regarding the mentioned EPSG numbers (the enum values), at the links below you may find some more information about "EPSG".
-    /// https://en.wikipedia.org/wiki/EPSG_Geodetic_Parameter_Dataset
-    /// https://epsg.org
-    /// https://epsg.io
-    /// </summary>
+/// <summary>
+/// Crs = Coordinate reference system.
+///
+/// The integer values for these enums are the EPSG numbers for the corresponding coordinate reference systems.
+/// There are three kind of coordinate systems supported and defined in this enum type below:
+///     WGS84
+///     SWEREF99 (the new Swedish grid, 13 versions, one national grid and 12 local projection zones)
+///     RT90 (the old Swedish grid, 6 local projection zones)
+/// There are extensions methods for the enum which can be used to determine one of the above three types.
+/// See also <see cref="CrsProjectionExtensions"/>
+///
+/// Regarding the mentioned EPSG numbers (the enum values), at the links below you may find some more information about "EPSG".
+/// https://en.wikipedia.org/wiki/EPSG_Geodetic_Parameter_Dataset
+/// https://epsg.org
+/// https://epsg.io
+/// </summary>
 """
 
 class CrsProjection(enum.Enum):
-            """
-            /// <summary>
-            /// https://epsg.org/crs_4326/WGS-84.html
-            /// https://epsg.io/4326
-            /// https://spatialreference.org/ref/epsg/4326/
-            /// https://en.wikipedia.org/wiki/World_Geodetic_System#A_new_World_Geodetic_System:_WGS_84
-            /// </summary>
-            """
-            wgs84 = 4326
+    """
+    /// <summary>
+    /// https://epsg.org/crs_4326/WGS-84.html
+    /// https://epsg.io/4326
+    /// https://spatialreference.org/ref/epsg/4326/
+    /// https://en.wikipedia.org/wiki/World_Geodetic_System#A_new_World_Geodetic_System:_WGS_84
+    /// </summary>
+    """
+    wgs84 = 4326
 
-            """
-            /// <summary>
-            /// "SWEREF 99 TM" (with EPSG code 3006) is the new national projection.
-            /// https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/gps-geodesi-och-swepos/referenssystem/tvadimensionella-system/sweref-99-projektioner/
-            /// https://epsg.org/crs_3006/SWEREF99-TM.html
-            /// https://epsg.io/3006
-            /// https://spatialreference.org/ref/epsg/3006/
-            /// </summary>
-            """
-            sweref_99_tm = 3006 # national sweref99 CRS
+    """
+    /// <summary>
+    /// "SWEREF 99 TM" (with EPSG code 3006) is the new national projection.
+    /// https://www.lantmateriet.se/sv/Kartor-och-geografisk-information/gps-geodesi-och-swepos/referenssystem/tvadimensionella-system/sweref-99-projektioner/
+    /// https://epsg.org/crs_3006/SWEREF99-TM.html
+    /// https://epsg.io/3006
+    /// https://spatialreference.org/ref/epsg/3006/
+    /// </summary>
+    """
+    sweref_99_tm = 3006 # national sweref99 CRS
 
-            """
-            // local sweref99 systems (the new swedish national system):
-            """
-            sweref_99_12_00 = 3007
-            sweref_99_13_30 = 3008
-            sweref_99_15_00 = 3009
-            sweref_99_16_30 = 3010
-            sweref_99_18_00 = 3011
-            sweref_99_14_15 = 3012
-            sweref_99_15_45 = 3013
-            sweref_99_17_15 = 3014
-            sweref_99_18_45 = 3015
-            sweref_99_20_15 = 3016
-            sweref_99_21_45 = 3017
-            sweref_99_23_15 = 3018
+    """
+    // local sweref99 systems (the new swedish national system):
+    """
+    sweref_99_12_00 = 3007
+    sweref_99_13_30 = 3008
+    sweref_99_15_00 = 3009
+    sweref_99_16_30 = 3010
+    sweref_99_18_00 = 3011
+    sweref_99_14_15 = 3012
+    sweref_99_15_45 = 3013
+    sweref_99_17_15 = 3014
+    sweref_99_18_45 = 3015
+    sweref_99_20_15 = 3016
+    sweref_99_21_45 = 3017
+    sweref_99_23_15 = 3018
 
-            """
-            // local RT90 systems (the old swedish national system):
-            """
-            rt90_7_5_gon_v = 3019
-            rt90_5_0_gon_v = 3020
+    """
+    // local RT90 systems (the old swedish national system):
+    """
+    rt90_7_5_gon_v = 3019
+    rt90_5_0_gon_v = 3020
 
-            """
-            /// <summary>
-            /// https://epsg.org/crs_3021/RT90-2-5-gon-V.html
-            /// https://epsg.io/3021
-            /// https://spatialreference.org/ref/epsg/3021/
-            /// </summary>
-            """
-            rt90_2_5_gon_v = 3021
+    """
+    /// <summary>
+    /// https://epsg.org/crs_3021/RT90-2-5-gon-V.html
+    /// https://epsg.io/3021
+    /// https://spatialreference.org/ref/epsg/3021/
+    /// </summary>
+    """
+    rt90_2_5_gon_v = 3021
 
-            rt90_0_0_gon_v = 3022
-            rt90_2_5_gon_o = 3023
-            rt90_5_0_gon_o = 3024
+    rt90_0_0_gon_v = 3022
+    rt90_2_5_gon_o = 3023
+    rt90_5_0_gon_o = 3024
 
-            def GetEpsgNumber(self) -> int :
-                # // the EPSG numbers have been used as the values in this enum
-                return self.value
+    def GetEpsgNumber(self) -> int :
+        # // the EPSG numbers have been used as the values in this enum
+        return self.value
 
-            def IsWgs84(self) -> bool :
-                return self.value == CrsProjection.wgs84.value
+    def IsWgs84(self) -> bool :
+        return self.value == CrsProjection.wgs84.value
 
-            def IsSweref(self) -> bool :
-                epsgNumber: int = self.GetEpsgNumber()
-                return _EpsgConstant.epsgLowerValueForSweref <= epsgNumber and epsgNumber <= _EpsgConstant.epsgUpperValueForSweref
+    def IsSweref(self) -> bool :
+        epsgNumber: int = self.GetEpsgNumber()
+        return _EpsgConstant.epsgLowerValueForSweref <= epsgNumber and epsgNumber <= _EpsgConstant.epsgUpperValueForSweref
 
-            def IsRT90(self) -> bool :
-                epsgNumber: int = self.GetEpsgNumber()
-                return _EpsgConstant.epsgLowerValueForRT90 <= epsgNumber and epsgNumber <= _EpsgConstant.epsgUpperValueForRT90
+    def IsRT90(self) -> bool :
+        epsgNumber: int = self.GetEpsgNumber()
+        return _EpsgConstant.epsgLowerValueForRT90 <= epsgNumber and epsgNumber <= _EpsgConstant.epsgUpperValueForRT90
 
 class _EpsgConstant:
     epsgLowerValueForSweref = 3006
--- tests/test_crs_projection.py
+++ tests/test_crs_projection.py
@@ -6,101 +6,100 @@ from sweden_crs_transformations.crs_projection import CrsProjection
 # using static SwedenCrsTransformationsTests.CrsProjectionFactoryTest; // to be able to use constants such as epsgNumberForSweref99tm and epsgNumberForWgs84
 # using static SwedenCrsTransformations.CrsProjection;
 
-
 class CrsProjectionTest(unittest.TestCase):
-        # TODO maybe define these below constants instead in 'CrsProjectionFactoryTest' as in C#.NET library being ported to this Python library
-        epsgNumberForWgs84: int = 4326
-        epsgNumberForSweref99tm: int = 3006
-        numberOfSweref99projections: int = 13  # ; // with EPSG numbers 3006-3018
-        numberOfRT90projections: int = 6  # ; // with EPSG numbers 3019-3024
-        numberOfWgs84Projections: int = 1  # ; // just to provide semantic instead of using a magic number 1 below
-        totalNumberOfProjections = numberOfSweref99projections + numberOfRT90projections + numberOfWgs84Projections
+    # TODO maybe define these below constants instead in 'CrsProjectionFactoryTest' as in C#.NET library being ported to this Python library
+    epsgNumberForWgs84: int = 4326
+    epsgNumberForSweref99tm: int = 3006
+    numberOfSweref99projections: int = 13  # ; // with EPSG numbers 3006-3018
+    numberOfRT90projections: int = 6  # ; // with EPSG numbers 3019-3024
+    numberOfWgs84Projections: int = 1  # ; // just to provide semantic instead of using a magic number 1 below
+    totalNumberOfProjections = numberOfSweref99projections + numberOfRT90projections + numberOfWgs84Projections
 
-        def setUp(self):
-            self._wgs84Projections = {CrsProjection.wgs84}
+    def setUp(self):
+        self._wgs84Projections = {CrsProjection.wgs84}
 
-            self._sweref99Projections = {
-                CrsProjection.sweref_99_12_00, CrsProjection.sweref_99_13_30, CrsProjection.sweref_99_14_15,
-                CrsProjection.sweref_99_15_00, CrsProjection.sweref_99_15_45, CrsProjection.sweref_99_16_30,
-                CrsProjection.sweref_99_17_15, CrsProjection.sweref_99_18_00, CrsProjection.sweref_99_18_45,
-                CrsProjection.sweref_99_20_15, CrsProjection.sweref_99_21_45, CrsProjection.sweref_99_23_15,
-                CrsProjection.sweref_99_tm
-            }
+        self._sweref99Projections = {
+            CrsProjection.sweref_99_12_00, CrsProjection.sweref_99_13_30, CrsProjection.sweref_99_14_15,
+            CrsProjection.sweref_99_15_00, CrsProjection.sweref_99_15_45, CrsProjection.sweref_99_16_30,
+            CrsProjection.sweref_99_17_15, CrsProjection.sweref_99_18_00, CrsProjection.sweref_99_18_45,
+            CrsProjection.sweref_99_20_15, CrsProjection.sweref_99_21_45, CrsProjection.sweref_99_23_15,
+            CrsProjection.sweref_99_tm
+        }
 
-            self._rt90Projections = {
-                CrsProjection.rt90_0_0_gon_v, CrsProjection.rt90_2_5_gon_o, CrsProjection.rt90_2_5_gon_v,
-                CrsProjection.rt90_5_0_gon_o, CrsProjection.rt90_5_0_gon_v, CrsProjection.rt90_7_5_gon_v
-            }
-
-
-        def test_getEpsgNumber(self):
-            self.assertEqual(
-                CrsProjectionTest.epsgNumberForSweref99tm,
-                CrsProjection.sweref_99_tm.GetEpsgNumber()
-            )
-
-            self.assertEqual(
-                CrsProjectionTest.epsgNumberForWgs84,
-                CrsProjection.wgs84.GetEpsgNumber()
-            )
+        self._rt90Projections = {
+            CrsProjection.rt90_0_0_gon_v, CrsProjection.rt90_2_5_gon_o, CrsProjection.rt90_2_5_gon_v,
+            CrsProjection.rt90_5_0_gon_o, CrsProjection.rt90_5_0_gon_v, CrsProjection.rt90_7_5_gon_v
+        }
 
 
-        def test_isWgs84(self):
-            self.assertEquals(
-                CrsProjectionTest.numberOfWgs84Projections,
-                len(self._wgs84Projections)
-            )
+    def test_getEpsgNumber(self):
+        self.assertEqual(
+            CrsProjectionTest.epsgNumberForSweref99tm,
+            CrsProjection.sweref_99_tm.GetEpsgNumber()
+        )
 
-            for crsProjection in self._wgs84Projections: #type: CrsProjection
-                self.assertTrue(crsProjection.IsWgs84())
-
-            for crsProjection in self._sweref99Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsWgs84())
-
-            for crsProjection in self._rt90Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsWgs84())
+        self.assertEqual(
+            CrsProjectionTest.epsgNumberForWgs84,
+            CrsProjection.wgs84.GetEpsgNumber()
+        )
 
 
-        def test_isSweRef99(self):
-            self.assertEquals(
-                CrsProjectionTest.numberOfSweref99projections,
-                len(self._sweref99Projections)
-            )
+    def test_isWgs84(self):
+        self.assertEquals(
+            CrsProjectionTest.numberOfWgs84Projections,
+            len(self._wgs84Projections)
+        )
 
-            for crsProjection in self._wgs84Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsSweref())
+        for crsProjection in self._wgs84Projections: #type: CrsProjection
+            self.assertTrue(crsProjection.IsWgs84())
 
-            for crsProjection in self._sweref99Projections: #type: CrsProjection
-                self.assertTrue(crsProjection.IsSweref())
+        for crsProjection in self._sweref99Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsWgs84())
 
-            for crsProjection in self._rt90Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsSweref())
+        for crsProjection in self._rt90Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsWgs84())
 
 
-        def test_isRT90(self):
-            self.assertEquals(
-                CrsProjectionTest.numberOfRT90projections,
-                len(self._rt90Projections)
-            )
+    def test_isSweRef99(self):
+        self.assertEquals(
+            CrsProjectionTest.numberOfSweref99projections,
+            len(self._sweref99Projections)
+        )
 
-            for crsProjection in self._wgs84Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsRT90())
+        for crsProjection in self._wgs84Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsSweref())
 
-            for crsProjection in self._sweref99Projections: #type: CrsProjection
-                self.assertFalse(crsProjection.IsRT90())
+        for crsProjection in self._sweref99Projections: #type: CrsProjection
+            self.assertTrue(crsProjection.IsSweref())
 
-            for crsProjection in self._rt90Projections: #type: CrsProjection
-                self.assertTrue(crsProjection.IsRT90())
+        for crsProjection in self._rt90Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsSweref())
 
-        '''
-        [Test]
-        public void CreateCoordinate() {
-            const double x = 22.5;
-            const double y = 62.5;
-            CrsCoordinate crsCoordinate = CrsProjection.sweref_99_tm.CreateCoordinate(y, x);
-            Assert.AreEqual(epsgNumberForSweref99tm, crsCoordinate.CrsProjection.GetEpsgNumber());
-            Assert.AreEqual(CrsProjection.sweref_99_tm, crsCoordinate.CrsProjection);
-            const double delta = 0.000001;
-            Assert.AreEqual(x, crsCoordinate.LongitudeX, delta);
-            Assert.AreEqual(y, crsCoordinate.LatitudeY, delta);
-        '''
+
+    def test_isRT90(self):
+        self.assertEquals(
+            CrsProjectionTest.numberOfRT90projections,
+            len(self._rt90Projections)
+        )
+
+        for crsProjection in self._wgs84Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsRT90())
+
+        for crsProjection in self._sweref99Projections: #type: CrsProjection
+            self.assertFalse(crsProjection.IsRT90())
+
+        for crsProjection in self._rt90Projections: #type: CrsProjection
+            self.assertTrue(crsProjection.IsRT90())
+
+    '''
+    [Test]
+    public void CreateCoordinate() {
+        const double x = 22.5;
+        const double y = 62.5;
+        CrsCoordinate crsCoordinate = CrsProjection.sweref_99_tm.CreateCoordinate(y, x);
+        Assert.AreEqual(epsgNumberForSweref99tm, crsCoordinate.CrsProjection.GetEpsgNumber());
+        Assert.AreEqual(CrsProjection.sweref_99_tm, crsCoordinate.CrsProjection);
+        const double delta = 0.000001;
+        Assert.AreEqual(x, crsCoordinate.LongitudeX, delta);
+        Assert.AreEqual(y, crsCoordinate.LatitudeY, delta);
+    '''
