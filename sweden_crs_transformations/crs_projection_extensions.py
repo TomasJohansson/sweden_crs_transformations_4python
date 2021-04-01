bc1e564c74f5120038b2a7da4a6ab2c54a8cbe8a
Tomas Johansson <github-tomas@programmerare.com>
2021-04-01 23:13:42 +0200

    Initial Python implementation for most of the code for 'CrsProjection' including the tests.

--- sweden_crs_transformations/crs_projection_extensions.py
+++ sweden_crs_transformations/crs_projection_extensions.py
@@ -1,76 +1,97 @@
-﻿/*
+﻿from sweden_crs_transformations.crs_projection import CrsProjection
+
+# this file has been ported from C# extension methods to Python monkey patching
+
+# TODO move these functions/methods directly into the Python 'class CrsProjection(enum.Enum)'
+
+"""
+/*
 * Copyright (c) Tomas Johansson , http://www.programmerare.com
 * The code in this library is licensed with MIT.
-* The library is based on the library 'MightyLittleGeodesy' (https://github.com/bjornsallarp/MightyLittleGeodesy/) 
+* The library is based on the library 'MightyLittleGeodesy' (https://github.com/bjornsallarp/MightyLittleGeodesy/)
 * which is also released with MIT.
 * License information about 'sweden_crs_transformations_4net' and 'MightyLittleGeodesy':
 * https://github.com/TomasJohansson/sweden_crs_transformations_4net/blob/csharpe_SwedenCrsTransformations/LICENSE
 * For more information see the webpage below.
 * https://github.com/TomasJohansson/sweden_crs_transformations_4net
 */
+"""
 
-namespace SwedenCrsTransformations {
-
+"""
     /// <summary>
     /// Extension methods for the enum CrsProjection.
     /// See also <see cref="CrsProjection"/>
     /// </summary>
-    public static class CrsProjectionExtensions {
+"""
+# class CrsProjectionExtensions:
 
-        private const int epsgForSweref99tm = 3006;
+# private const int epsgForSweref99tm = 3006;
+epsgForSweref99tm: int = 3006
 
-        //private const int epsgLowerValueForSwerefLocal = 3007; // the NATIONAL sweref99TM has value 3006 as in the above constant
-        //private const int epsgUpperValueForSwerefLocal = 3018;
-        private const int epsgLowerValueForSweref = epsgForSweref99tm;
-        private const int epsgUpperValueForSweref = 3018;
+# //private const int epsgLowerValueForSwerefLocal = 3007; // the NATIONAL sweref99TM has value 3006 as in the above constant
+# //private const int epsgUpperValueForSwerefLocal = 3018;
+# private const int epsgLowerValueForSweref = epsgForSweref99tm;
+# private const int epsgUpperValueForSweref = 3018;
+epsgLowerValueForSweref: int = epsgForSweref99tm
+epsgUpperValueForSweref: int = 3018
 
-        private const int epsgLowerValueForRT90 = 3019;
-        private const int epsgUpperValueForRT90 = 3024;
 
-        /// <summary>
-        /// The EPSG number for the enum instance representing a coordinate reference system.
-        /// The implementation is trivial but it is a convenience method that provides semantic 
-        /// through the method name i.e. what the enum value represents 
-        /// and it also lets the client code avoid to do the casting.
-        /// </summary>
-        /// <returns>
-        /// An EPSG number.
-        /// https://en.wikipedia.org/wiki/EPSG_Geodetic_Parameter_Dataset
-        /// </returns>
-        public static int GetEpsgNumber(this CrsProjection crsProjection) { 
-            // the EPSG numbers have been used as the values in this enum
-            return (int)crsProjection;
-        }
+# private const int epsgLowerValueForRT90 = 3019;
+# private const int epsgUpperValueForRT90 = 3024;
+epsgLowerValueForRT90: int = 3019
+epsgUpperValueForRT90: int = 3024
 
-        /// <summary>
-        /// True if the coordinate reference system is WGS84.
-        /// </summary>
-        public static bool IsWgs84(this CrsProjection crsProjection) {
-            return crsProjection == CrsProjection.wgs84;
-        }
+"""
+/// <summary>
+/// The EPSG number for the enum instance representing a coordinate reference system.
+/// The implementation is trivial but it is a convenience method that provides semantic
+/// through the method name i.e. what the enum value represents
+/// and it also lets the client code avoid to do the casting.
+/// </summary>
+/// <returns>
+/// An EPSG number.
+/// https://en.wikipedia.org/wiki/EPSG_Geodetic_Parameter_Dataset
+/// </returns>
+"""
+def GetEpsgNumber(self) -> int :
+    # // the EPSG numbers have been used as the values in this enum
+    return self.value
+CrsProjection.GetEpsgNumber = GetEpsgNumber
 
-        /// <summary>
-        /// True if the coordinate reference system is a version of SWEREF99.
-        /// </summary>
-        public static bool IsSweref(this CrsProjection crsProjection) {
-            int epsgNumber = crsProjection.GetEpsgNumber();
-            return epsgLowerValueForSweref <= epsgNumber && epsgNumber <= epsgUpperValueForSweref;
-        }
+"""
+/// <summary>
+/// True if the coordinate reference system is WGS84.
+/// </summary>
+"""
+def isWgs84(self) -> bool :
+    return self.value == CrsProjection.wgs84.value
+CrsProjection.IsWgs84 = isWgs84
 
-        /// <summary>
-        /// True if the coordinate reference system is a version of RT90.
-        /// </summary>
-        public static bool IsRT90(this CrsProjection crsProjection) {
-            int epsgNumber = crsProjection.GetEpsgNumber();
-            return epsgLowerValueForRT90 <= epsgNumber && epsgNumber <= epsgUpperValueForRT90;
-        }
+"""
+/// <summary>
+/// True if the coordinate reference system is a version of SWEREF99.
+/// </summary>
+"""
+def isSweref(self) -> bool :
+    epsgNumber: int = self.GetEpsgNumber()
+    return epsgLowerValueForSweref <= epsgNumber and epsgNumber <= epsgUpperValueForSweref
+CrsProjection.IsSweref = isSweref
 
-        public static CrsCoordinate CreateCoordinate(
-            this CrsProjection crsProjection,
-            double yLatitude,
-            double xLongitude
-        ) {
-            return CrsCoordinate.CreateCoordinate(crsProjection, yLatitude, xLongitude);
-        }
-    }
-}
+"""
+/// <summary>
+/// True if the coordinate reference system is a version of RT90.
+/// </summary>
+"""
+def isRT90(self) -> bool :
+    epsgNumber: int = self.GetEpsgNumber()
+    return epsgLowerValueForRT90 <= epsgNumber and epsgNumber <= epsgUpperValueForRT90
+CrsProjection.IsRT90 = isRT90
+
+'''
+public static CrsCoordinate CreateCoordinate(
+    this CrsProjection crsProjection,
+    double yLatitude,
+    double xLongitude
+) {
+    return CrsCoordinate.CreateCoordinate(crsProjection, yLatitude, xLongitude);
+'''
--- tests/test_crs_projection.py
+++ tests/test_crs_projection.py
@@ -1,91 +1,102 @@
-﻿using NUnit.Framework;
-using SwedenCrsTransformations;
-using System.Collections.Generic;
-using static SwedenCrsTransformationsTests.CrsProjectionFactoryTest; // to be able to use constants such as epsgNumberForSweref99tm and epsgNumberForWgs84
-using static SwedenCrsTransformations.CrsProjection;
-namespace SwedenCrsTransformationsTests {
-    
-    [TestFixture]
-    public class CrsProjectionExtensionsTest {
+﻿import unittest
+from sweden_crs_transformations.crs_projection import CrsProjection
 
-        private HashSet<CrsProjection> _wgs84Projections;
-        private HashSet<CrsProjection> _sweref99Projections;
-        private HashSet<CrsProjection> _rt90Projections;
+# the below imported file has been ported from C# extension methods to Python monkey patching
+import sweden_crs_transformations.crs_projection_extensions
+# TODO move the functions/methods in the above file directly into the Python 'class CrsProjection(enum.Enum)'
 
-        [SetUp]
-        public void SetUp() {
-            _wgs84Projections = new HashSet<CrsProjection>{ wgs84 };
-            _sweref99Projections = new HashSet<CrsProjection>{ 
-                sweref_99_12_00, sweref_99_13_30, sweref_99_14_15,
-                sweref_99_15_00, sweref_99_15_45, sweref_99_16_30,
-                sweref_99_17_15, sweref_99_18_00, sweref_99_18_45,
-                sweref_99_20_15, sweref_99_21_45, sweref_99_23_15, sweref_99_tm
-            };
-            _rt90Projections = new HashSet<CrsProjection>{ 
-                rt90_0_0_gon_v, rt90_2_5_gon_o, rt90_2_5_gon_v,
-                rt90_5_0_gon_o, rt90_5_0_gon_v, rt90_7_5_gon_v
-            };
-        }
+# using SwedenCrsTransformations;
+# using System.Collections.Generic;
+# using static SwedenCrsTransformationsTests.CrsProjectionFactoryTest; // to be able to use constants such as epsgNumberForSweref99tm and epsgNumberForWgs84
+# using static SwedenCrsTransformations.CrsProjection;
 
-        [Test]
-        public void GetEpsgNumber() {
-            Assert.AreEqual(
-                epsgNumberForSweref99tm, // constant defined in CrsProjectionFactoryTest
+
+class CrsProjectionTest(unittest.TestCase):
+        # TODO maybe define these below constants instead in 'CrsProjectionFactoryTest' as in C#.NET library being ported to this Python library
+        epsgNumberForWgs84: int = 4326
+        epsgNumberForSweref99tm: int = 3006
+        numberOfSweref99projections: int = 13  # ; // with EPSG numbers 3006-3018
+        numberOfRT90projections: int = 6  # ; // with EPSG numbers 3019-3024
+        numberOfWgs84Projections: int = 1  # ; // just to provide semantic instead of using a magic number 1 below
+        totalNumberOfProjections = numberOfSweref99projections + numberOfRT90projections + numberOfWgs84Projections
+
+        def setUp(self):
+            self._wgs84Projections = {CrsProjection.wgs84}
+
+            self._sweref99Projections = {
+                CrsProjection.sweref_99_12_00, CrsProjection.sweref_99_13_30, CrsProjection.sweref_99_14_15,
+                CrsProjection.sweref_99_15_00, CrsProjection.sweref_99_15_45, CrsProjection.sweref_99_16_30,
+                CrsProjection.sweref_99_17_15, CrsProjection.sweref_99_18_00, CrsProjection.sweref_99_18_45,
+                CrsProjection.sweref_99_20_15, CrsProjection.sweref_99_21_45, CrsProjection.sweref_99_23_15,
+                CrsProjection.sweref_99_tm
+            }
+
+            self._rt90Projections = {
+                CrsProjection.rt90_0_0_gon_v, CrsProjection.rt90_2_5_gon_o, CrsProjection.rt90_2_5_gon_v,
+                CrsProjection.rt90_5_0_gon_o, CrsProjection.rt90_5_0_gon_v, CrsProjection.rt90_7_5_gon_v
+            }
+
+
+        def test_getEpsgNumber(self):
+            self.assertEqual(
+                CrsProjectionTest.epsgNumberForSweref99tm,
                 CrsProjection.sweref_99_tm.GetEpsgNumber()
-            );
+            )
 
-            Assert.AreEqual(
-                epsgNumberForWgs84, // constant defined in CrsProjectionFactoryTest
+            self.assertEqual(
+                CrsProjectionTest.epsgNumberForWgs84,
                 CrsProjection.wgs84.GetEpsgNumber()
-            );
-        }
+            )
 
 
-        [Test]
-        public void isWgs84() {
-            Assert.AreEqual(numberOfWgs84Projections, _wgs84Projections.Count);
+        def test_isWgs84(self):
+            self.assertEquals(
+                CrsProjectionTest.numberOfWgs84Projections,
+                len(self._wgs84Projections)
+            )
 
-            foreach(var item in _wgs84Projections) {
-                Assert.IsTrue(item.IsWgs84());
-            }
-            foreach(var item in _sweref99Projections) {
-                Assert.IsFalse(item.IsWgs84());
-            }
-            foreach(var item in _rt90Projections) {
-                Assert.IsFalse(item.IsWgs84());
-            }
-        }
+            for crsProjection in self._wgs84Projections: #type: CrsProjection
+                self.assertTrue(crsProjection.IsWgs84())
 
-        [Test]
-        public void isSweref() {
-            Assert.AreEqual(numberOfSweref99projections, _sweref99Projections.Count);
+            for crsProjection in self._sweref99Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsWgs84())
 
-            foreach(var item in _wgs84Projections) {
-                Assert.IsFalse(item.IsSweref());
-            }
-            foreach(var item in _sweref99Projections) {
-                Assert.IsTrue(item.IsSweref());
-            }
-            foreach(var item in _rt90Projections) {
-                Assert.IsFalse(item.IsSweref());
-            }
-        }
+            for crsProjection in self._rt90Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsWgs84())
 
-        [Test]
-        public void isRT90() {
-            Assert.AreEqual(numberOfRT90projections, _rt90Projections.Count);
 
-            foreach(var item in _wgs84Projections) {
-                Assert.IsFalse(item.IsRT90());
-            }
-            foreach(var item in _sweref99Projections) {
-                Assert.IsFalse(item.IsRT90());
-            }
-            foreach(var item in _rt90Projections) {
-                Assert.IsTrue(item.IsRT90());
-            }
-        }
+        def test_isSweRef99(self):
+            self.assertEquals(
+                CrsProjectionTest.numberOfSweref99projections,
+                len(self._sweref99Projections)
+            )
 
+            for crsProjection in self._wgs84Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsSweref())
+
+            for crsProjection in self._sweref99Projections: #type: CrsProjection
+                self.assertTrue(crsProjection.IsSweref())
+
+            for crsProjection in self._rt90Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsSweref())
+
+
+        def test_isRT90(self):
+            self.assertEquals(
+                CrsProjectionTest.numberOfRT90projections,
+                len(self._rt90Projections)
+            )
+
+            for crsProjection in self._wgs84Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsRT90())
+
+            for crsProjection in self._sweref99Projections: #type: CrsProjection
+                self.assertFalse(crsProjection.IsRT90())
+
+            for crsProjection in self._rt90Projections: #type: CrsProjection
+                self.assertTrue(crsProjection.IsRT90())
+
+        '''
         [Test]
         public void CreateCoordinate() {
             const double x = 22.5;
@@ -96,6 +107,4 @@ namespace SwedenCrsTransformationsTests {
             const double delta = 0.000001;
             Assert.AreEqual(x, crsCoordinate.LongitudeX, delta);
             Assert.AreEqual(y, crsCoordinate.LatitudeY, delta);
-        }
-    }
-}
+        '''
