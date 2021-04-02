123f4f79c36f75ead70c4f3979943a10b7fde7ec
Tomas Johansson <github-tomas@programmerare.com>
2021-04-02 23:13:08 +0200

    Only indentation. Nothing else in this git commit.

--- sweden_crs_transformations/crs_coordinate.py
+++ sweden_crs_transformations/crs_coordinate.py
@@ -24,151 +24,151 @@ from sweden_crs_transformations.crs_projection import CrsProjection
 """
 class CrsCoordinate:
 
-        """
-        /// <summary>
-        /// The coordinate reference system that defines the location together with the other two properties (LongitudeX and LatitudeY).
-        /// </summary>
-        """
-        # public CrsProjection CrsProjection { get; private set; }
-        def get_crs_projection(self) -> CrsProjection:
-            return self.crsProjection
+    """
+    /// <summary>
+    /// The coordinate reference system that defines the location together with the other two properties (LongitudeX and LatitudeY).
+    /// </summary>
+    """
+    # public CrsProjection CrsProjection { get; private set; }
+    def get_crs_projection(self) -> CrsProjection:
+        return self.crsProjection
 
-        """
-        /// <summary>
-        /// The coordinate value representing the longitude or X or Easting.
-        /// </summary>
-        """
-        # public double LongitudeX { get; private set; }
-        def get_longitude_x(self) -> float:
-            return self.xLongitude
-
-
-        """
-        /// <summary>
-        /// The coordinate value representing the latitude or Y or Northing.
-        /// </summary>
-        """
-        # public double LatitudeY { get; private set; }
-        def get_latitude_y(self) -> float:
-            return self.yLatitude
-
-        """
-        /// <summary>
-        /// Private constructor. Client code must instead use the public factory methods.
-        /// </summary>
-        """
-        def __init__(self, crsProjection: CrsProjection, yLatitude: float, xLongitude: float):
-            self.crsProjection = crsProjection
-            self.yLatitude = yLatitude
-            self.xLongitude = xLongitude
+    """
+    /// <summary>
+    /// The coordinate value representing the longitude or X or Easting.
+    /// </summary>
+    """
+    # public double LongitudeX { get; private set; }
+    def get_longitude_x(self) -> float:
+        return self.xLongitude
 
 
-        """
-        /// <summary>
-        /// Transforms the coordinate to another coordinate reference system
-        /// </summary>
-        /// <param name="targetCrsProjection">the coordinate reference system that you want to transform to</param>
-        """
-        def transform(self, targetCrsProjection: CrsProjection) -> CrsCoordinate:
-            from sweden_crs_transformations.transformation.transformer import Transformer
-            return Transformer.transform(self, targetCrsProjection)
+    """
+    /// <summary>
+    /// The coordinate value representing the latitude or Y or Northing.
+    /// </summary>
+    """
+    # public double LatitudeY { get; private set; }
+    def get_latitude_y(self) -> float:
+        return self.yLatitude
 
-        """
-        /// <summary>
-        /// Transforms the coordinate to another coordinate reference system
-        /// </summary>
-        /// <param name="targetEpsgNumber">the coordinate reference system that you want to transform to</param>
-        """
-        def transform_by_epsg_number(self, targetEpsgNumber: int) -> CrsCoordinate:
-            targetCrsProjection: CrsProjection = CrsProjectionFactory.get_crs_projection_by_epsg_number(targetEpsgNumber)
-            return self.transform(targetCrsProjection)
+    """
+    /// <summary>
+    /// Private constructor. Client code must instead use the public factory methods.
+    /// </summary>
+    """
+    def __init__(self, crsProjection: CrsProjection, yLatitude: float, xLongitude: float):
+        self.crsProjection = crsProjection
+        self.yLatitude = yLatitude
+        self.xLongitude = xLongitude
 
 
-        """
-        /// <summary>
-        /// Factory method for creating an instance.
-        /// </summary>
-        /// <param name="epsgNumber">represents the coordinate reference system that defines the location together with the other two parameters</param>
-        /// <param name="xLongitude">the coordinate position value representing the longitude or X or Easting</param>
-        /// <param name="yLatitude">the coordinate position value representing the latitude or Y or Northing</param>
-        """
+    """
+    /// <summary>
+    /// Transforms the coordinate to another coordinate reference system
+    /// </summary>
+    /// <param name="targetCrsProjection">the coordinate reference system that you want to transform to</param>
+    """
+    def transform(self, targetCrsProjection: CrsProjection) -> CrsCoordinate:
+        from sweden_crs_transformations.transformation.transformer import Transformer
+        return Transformer.transform(self, targetCrsProjection)
 
-        @classmethod
-        def create_coordinate_by_epsg_number(cls,
-            epsgNumber: int,
-            yLatitude: float,
-            xLongitude: float
-        ) -> CrsCoordinate:
-            crsProjection: CrsProjection = CrsProjectionFactory.get_crs_projection_by_epsg_number(epsgNumber)
-            return cls.create_coordinate(crsProjection, yLatitude, xLongitude)
+    """
+    /// <summary>
+    /// Transforms the coordinate to another coordinate reference system
+    /// </summary>
+    /// <param name="targetEpsgNumber">the coordinate reference system that you want to transform to</param>
+    """
+    def transform_by_epsg_number(self, targetEpsgNumber: int) -> CrsCoordinate:
+        targetCrsProjection: CrsProjection = CrsProjectionFactory.get_crs_projection_by_epsg_number(targetEpsgNumber)
+        return self.transform(targetCrsProjection)
 
-        """
-        /// <summary>
-        /// Factory method for creating an instance.
-        /// See also <see cref="CrsProjection"/>
-        /// </summary>
-        /// <param name="crsProjection">represents the coordinate reference system that defines the location together with the other two parameters</param>
-        /// <param name="xLongitude">the coordinate position value representing the longitude or X or Easting</param>
-        /// <param name="yLatitude">the coordinate position value representing the latitude or Y or Northing</param>
-        """
 
-        @classmethod
-        def create_coordinate(cls,
-            crsProjection: CrsProjection,
-            yLatitude: float,
-            xLongitude: float
-        ) -> CrsCoordinate:
-            return CrsCoordinate(crsProjection, yLatitude, xLongitude)
+    """
+    /// <summary>
+    /// Factory method for creating an instance.
+    /// </summary>
+    /// <param name="epsgNumber">represents the coordinate reference system that defines the location together with the other two parameters</param>
+    /// <param name="xLongitude">the coordinate position value representing the longitude or X or Easting</param>
+    /// <param name="yLatitude">the coordinate position value representing the latitude or Y or Northing</param>
+    """
 
-        """
-        # // ----------------------------------------------------------------------------------------------------------------------
-        # // These five methods below was generated with Visual Studio 2019
-        """
+    @classmethod
+    def create_coordinate_by_epsg_number(cls,
+        epsgNumber: int,
+        yLatitude: float,
+        xLongitude: float
+    ) -> CrsCoordinate:
+        crsProjection: CrsProjection = CrsProjectionFactory.get_crs_projection_by_epsg_number(epsgNumber)
+        return cls.create_coordinate(crsProjection, yLatitude, xLongitude)
 
-        """
-        public override bool Equals(object obj) {
-            return Equals(obj as CrsCoordinate);
-        }
+    """
+    /// <summary>
+    /// Factory method for creating an instance.
+    /// See also <see cref="CrsProjection"/>
+    /// </summary>
+    /// <param name="crsProjection">represents the coordinate reference system that defines the location together with the other two parameters</param>
+    /// <param name="xLongitude">the coordinate position value representing the longitude or X or Easting</param>
+    /// <param name="yLatitude">the coordinate position value representing the latitude or Y or Northing</param>
+    """
 
-        public bool Equals(CrsCoordinate other) {
-            return other != null &&
-                   CrsProjection == other.CrsProjection &&
-                   LongitudeX == other.LongitudeX &&
-                   LatitudeY == other.LatitudeY;
-        }
+    @classmethod
+    def create_coordinate(cls,
+        crsProjection: CrsProjection,
+        yLatitude: float,
+        xLongitude: float
+    ) -> CrsCoordinate:
+        return CrsCoordinate(crsProjection, yLatitude, xLongitude)
 
-        public override int GetHashCode() {
-            int hashCode = 1147467376;
-            hashCode = hashCode * -1521134295 + CrsProjection.GetHashCode();
-            hashCode = hashCode * -1521134295 + LongitudeX.GetHashCode();
-            hashCode = hashCode * -1521134295 + LatitudeY.GetHashCode();
-            return hashCode;
-        }
+    """
+    # // ----------------------------------------------------------------------------------------------------------------------
+    # // These five methods below was generated with Visual Studio 2019
+    """
 
-        public static bool operator ==(CrsCoordinate left, CrsCoordinate right) {
-            return EqualityComparer<CrsCoordinate>.Default.Equals(left, right);
-        }
+    """
+    public override bool Equals(object obj) {
+        return Equals(obj as CrsCoordinate);
+    }
 
-        public static bool operator !=(CrsCoordinate left, CrsCoordinate right) {
-            return !(left == right);
-        }
-        """
-        #// These five methods above was generated with Visual Studio 2019
-        # // ----------------------------------------------------------------------------------------------------------------------
+    public bool Equals(CrsCoordinate other) {
+        return other != null &&
+               CrsProjection == other.CrsProjection &&
+               LongitudeX == other.LongitudeX &&
+               LatitudeY == other.LatitudeY;
+    }
 
-        """
-        /// <summary>
-        /// Two examples of the string that can be returned:
-        /// "CrsCoordinate [ X: 153369.673 , Y: 6579457.649 , CRS: SWEREF_99_18_00 ]"
-        /// "CrsCoordinate [ Longitude: 18.059196 , Latitude: 59.330231 , CRS: WGS84 ]"
-        /// </summary>
-        """
-        def __str__(self):
-            crs: str = str(self.get_crs_projection()).upper()
-            isWgs84: bool = self.get_crs_projection().is_wgs84()
-            yOrLatitude: str = "Y"
-            xOrLongitude: str = "X"
-            if (isWgs84):
-                yOrLatitude = "Latitude"
-                xOrLongitude = "Longitude"
-            return f"CrsCoordinate [ {yOrLatitude}: {self.get_latitude_y()} , {xOrLongitude}: {self.get_longitude_x()} , CRS: {crs} ]"
+    public override int GetHashCode() {
+        int hashCode = 1147467376;
+        hashCode = hashCode * -1521134295 + CrsProjection.GetHashCode();
+        hashCode = hashCode * -1521134295 + LongitudeX.GetHashCode();
+        hashCode = hashCode * -1521134295 + LatitudeY.GetHashCode();
+        return hashCode;
+    }
+
+    public static bool operator ==(CrsCoordinate left, CrsCoordinate right) {
+        return EqualityComparer<CrsCoordinate>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(CrsCoordinate left, CrsCoordinate right) {
+        return !(left == right);
+    }
+    """
+    #// These five methods above was generated with Visual Studio 2019
+    # // ----------------------------------------------------------------------------------------------------------------------
+
+    """
+    /// <summary>
+    /// Two examples of the string that can be returned:
+    /// "CrsCoordinate [ X: 153369.673 , Y: 6579457.649 , CRS: SWEREF_99_18_00 ]"
+    /// "CrsCoordinate [ Longitude: 18.059196 , Latitude: 59.330231 , CRS: WGS84 ]"
+    /// </summary>
+    """
+    def __str__(self):
+        crs: str = str(self.get_crs_projection()).upper()
+        isWgs84: bool = self.get_crs_projection().is_wgs84()
+        yOrLatitude: str = "Y"
+        xOrLongitude: str = "X"
+        if (isWgs84):
+            yOrLatitude = "Latitude"
+            xOrLongitude = "Longitude"
+        return f"CrsCoordinate [ {yOrLatitude}: {self.get_latitude_y()} , {xOrLongitude}: {self.get_longitude_x()} , CRS: {crs} ]"
