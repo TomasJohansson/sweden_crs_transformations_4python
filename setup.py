084767f942d765cc60087c394c5dc800e8b4c5c2
Tomas Johansson <github-tomas@programmerare.com>
2021-04-10 10:00:29 +0200

    Updated the files setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
@@ -19,30 +19,29 @@ test_requirements = [ ]
 setup(
     author="Tomas Johansson",
     author_email='pypi@programmerare.com',
-    python_requires='>=3.5',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Topic :: Scientific/Engineering :: GIS",
+        'Programming Language :: Python :: 3.9',
     ],
     description="Python library for transformation of geographic coordinates between WGS84 and the Swedish coordinate reference systems SWEREF99 and RT90",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='sweden_crs_transformations',
-    name='sweden_crs_transformations',
+    name='sweden_crs_transformations-programmerare',  # name at 'test.pypi.org'
+    # name='sweden_crs_transformations',  # name at 'pypi.org'
     packages=find_packages(include=['sweden_crs_transformations', 'sweden_crs_transformations.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/TomasJohansson/sweden_crs_transformations',
+    url='https://github.com/TomasJohansson/sweden_crs_transformations_4python',
     version='0.1.0',
     zip_safe=False,
 )
